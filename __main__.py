203617d47f905f8de825f008b179f0ded2f48088
Naokitsu <naokitsu@tutanota.com>
2022-11-24 14:38:40 +0300

    Comment out len argv check

--- __main__.py
+++ __main__.py
@@ -1,12 +1,18 @@
 import sys
 
+from pydriller import Repository, Git
+
+
 def main():
     """
     Точка входа
     """
+    """
     if len(sys.argv) != 2:
         print("Передайте репозиторий первым аргументом")
         return
+    """
+
 
 if __name__ == '__main__':
     main()
