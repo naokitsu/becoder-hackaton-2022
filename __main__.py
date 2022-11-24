3e83ded018cbd8275bbc75c63f236c7e755808be
Naokitsu <naokitsu@tutanota.com>
2022-11-24 11:23:37 +0300

    Create main file

---
+++ __main__.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+    """
+    Точка входа
+    """
+    if len(sys.argv) != 2:
+        print("Передайте репозиторий первым аргументом")
+        return
+
+if __name__ == '__main__':
+    main()
