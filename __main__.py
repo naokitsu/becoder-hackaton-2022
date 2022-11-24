0b36c3c60dfe9c8f4719719bbcdd9fb8cdd64abe
Naokitsu <naokitsu@tutanota.com>
2022-11-24 15:52:04 +0300

    Commits by {user} that were fixed

--- __main__.py
+++ __main__.py
@@ -12,16 +12,30 @@ def main():
         print("Передайте репозиторий первым аргументом")
         return
     """
-py
     gr = Git("/home/narinai/Downloads/memos")
+
+    email_files = {}
+
     for commit in Repository('/home/narinai/Downloads/memos').traverse_commits():
-        if not("fix" in commit.msg):
+        if not ("fix" in commit.msg):
             continue
-        print('Hash: {} \t\t\t Author:\t {} \t\t\t Message:\t {}'.format(commit.hash, commit.author.name, commit.msg))
+
         gr_commit = gr.get_commit(commit.hash)
         buggy_commits = gr.get_commits_last_modified_lines(gr_commit)
-        print(buggy_commits)
-        input()
+        for i in buggy_commits:
+            for j in buggy_commits[i]:
+                buggy_commit = gr.get_commit(j)
+                if not(buggy_commit.author.email in email_files):
+                    email_files[buggy_commit.author.email] = {}
+                if not(i in email_files[buggy_commit.author.email]):
+                    email_files[buggy_commit.author.email][i] = 0
+                email_files[buggy_commit.author.email][i] += 1
+
+    for user in email_files:
+        print(f"\nCommits by {user} that were fixed:")
+        for file in email_files[user]:
+            print(f"{email_files[user][file]} times \t\t\t {file}")
+
 
 if __name__ == '__main__':
     main()
